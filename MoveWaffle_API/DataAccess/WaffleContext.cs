587facb5d6a8f69435ae1de37df4aba55b28b5ba
Emil Skov Olesen <emil8250@ymail.com>
2021-03-16 20:48:59 +0100

    Added EntityFramework migrations for the waffle database.

---
+++ MoveWaffle_API/DataAccess/WaffleContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using MoveWaffle_API.Models;
+
+namespace MoveWaffle_API.DataAccess
+{
+    public class WaffleContext:DbContext
+    {
+        public WaffleContext(DbContextOptions<WaffleContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
