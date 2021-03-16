fc056c8a2604052f845c223deaf8f8c010b4c2a6
Tenasow <bjarke.mouritzen@gmail.com>
2021-03-16 22:50:11 +0100

    Moved code from deleted file.

--- MoveWaffle_API/Implementation/Reader.cs
+++ MoveWaffle_API/Implementation/Reader.cs
@@ -36,5 +36,51 @@ namespace MoveWaffle_API.Implementation
             //throw new NotImplementedException();
             return _dbContext.Set<User>();
         }
+
+        public List<IMDBTitle> GetTitles()
+        {
+            List<IMDBTitle> resultTitles = new List<IMDBTitle>();
+
+            string[] lines = File.ReadAllLines(@"D:\programming\movie-waffle\MoveWaffle_API\IMDB-datasets\title.basics.tsv");
+
+            System.Diagnostics.Debug.WriteLine(lines.Length);
+
+            foreach (string row in lines.Skip(1))
+            {
+
+                string[] epVal = row.Split('\t');
+                List<string> genres = new List<string>(epVal[8].Split(','));
+
+                try
+                {
+                    System.Diagnostics.Debug.WriteLine(row);
+
+                    DateTime? startDate;
+                    DateTime? endDate;
+
+                    if (epVal[5] == "\\N")
+                        startDate = null;
+                    else
+                        startDate = new DateTime(int.Parse(epVal[5]));
+
+                    if (epVal[6] == "\\N")
+                        endDate = null;
+                    else
+                        endDate = new DateTime(int.Parse(epVal[6]));
+
+
+                    IMDBTitle Episode = new IMDBTitle(epVal[0], epVal[1], epVal[2], epVal[3],
+                        epVal[4] == "0" ? true : false, startDate, endDate, epVal[7], genres);
+                    resultTitles.Add(Episode);
+                }
+                catch (System.FormatException)
+                {
+
+                    throw;
+                }
+            }
+
+            return resultTitles;
+        }
     }
 }
